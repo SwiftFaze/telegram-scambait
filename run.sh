d890c686234b64a07db83445657c90a050206b08
SwiftFaze <64843163+SwiftFaze@users.noreply.github.com>
2025-10-12 15:36:46 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
-# Kill any old instances first, then start fresh
-ps aux | grep '[m]ain.py' | awk '{print $2}' | xargs -r kill
-nohup python3 /home/debian/telegram-scambait/main.py > /home/debian/telegram-scambait/main.log 2>&1 &
+#!/bin/bash
+# Always use full paths in cron scripts
+
+# Set working directory
+cd /home/debian/telegram-scambait || exit 1
+
+# Kill old instances
+/usr/bin/ps aux | /usr/bin/grep '[m]ain.py' | /usr/bin/awk '{print $2}' | /usr/bin/xargs -r /bin/kill
+
+# Start new instance in background
+/usr/bin/nohup /usr/bin/python3 /home/debian/telegram-scambait/main.py > /dev/null 2>&1 &
